d3298f8ca6d42076a37ddd600ed7f582c36fe169
若水 <ruoshui2533@gmail.com>
2019-03-08 18:48:47 +0800

    Add files via upload

---
+++ HelloImpl.java
@@ -0,0 +1,12 @@
+//HelloImpl.java
+import javax.rmi.PortableRemoteObject;
+
+public class HelloImpl extends PortableRemoteObject implements HelloInterface {
+  public HelloImpl() throws java.rmi.RemoteException {
+    super();     // invoke rmi linking and remote object initialization
+  }
+
+  public void sayHello() throws java.rmi.RemoteException {
+    System.out.println( "It works!  Hello World!!" );
+  }
+}
